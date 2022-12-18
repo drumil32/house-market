4fbea690b9d1d1b8da16b7bbcc714a6743ea1b31
Drumil_Akhenia <akheniad@gmail.com>
2022-12-18 15:32:02 +0530

    added components and pages
    components: - navbar
    pages:- offers, profile, signin, signup, explore, forgotpassword

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Profile from './pages/Profile';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div >
-      hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navbar />} >
+          <Route path="/" element={<Explore />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+        </Route>
+      </Routes>
+    </BrowserRouter >
   );
 }
 
